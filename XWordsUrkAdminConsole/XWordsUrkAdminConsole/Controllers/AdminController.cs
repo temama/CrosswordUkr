3f67f27f4c0848129ff1bb21bacb6dc4a4c022fe
temama <rtemka@gmail.com>
2017-08-31 23:21:23 +0300

    EOD commit

---
+++ XWordsUrkAdminConsole/XWordsUrkAdminConsole/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using XWordsUrkAdminConsole.Models;
+using System.Web.Mvc;
+using System.Linq;
+
+namespace XWordsUrkAdminConsole.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Words()
+        {
+            using (var context = new CrosswordsUkrAdminDataContainer())
+            {
+                return View(context.WordsSet.ToList());
+            }
+        }
+    }
+}
--- XWordsUrkAdminConsole/XWordsUrkAdminConsole/Controllers/HomeController.cs
+++ XWordsUrkAdminConsole/XWordsUrkAdminConsole/Controllers/HomeController.cs
@@ -15,16 +15,12 @@ namespace XWordsUrkAdminConsole.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            return PartialView();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return PartialView();
         }
     }
 }
