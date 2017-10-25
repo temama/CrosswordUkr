c273480dda6ccbe3ec6708f1cf3d6ce64f1fab45
temama <rtemka@gmail.com>
2017-10-25 20:13:35 +0300

    Games placeholder added

---
+++ XWordsUrkAdminConsole/XWordsUrkAdminConsole/Controllers/GamesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XWordsUrkAdminConsole.Accounting;
+
+namespace XWordsUrkAdminConsole.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games
+        public ActionResult Index()
+        {
+            var reqUser = AuthModule.GetCurrentUser(Request, true, Response);
+            if (reqUser == null)
+                return RedirectToAction("Login", "Home", new { ReturnUrl = Request.RawUrl });
+
+            return View();
+        }
+
+        public ActionResult Games()
+        {
+            var reqUser = AuthModule.GetCurrentUser(Request, true, Response);
+            if (reqUser == null)
+                return RedirectToAction("Login", "Home", new { ReturnUrl = Request.RawUrl });
+
+            return View("Games");
+        }
+    }
+}
